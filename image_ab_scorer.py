16abc15ac8ebc17e2fd93b7782fa5b18fd8d591d
Chris <chris.goringe@gmail.com>
2024-02-13 16:48:58 +1100

    arguments

--- image_ab_scorer.py
+++ image_ab_scorer.py
@@ -1,39 +1,38 @@
-import random, math, time
+import random, math, time, argparse
 import customtkinter
 import scipy
 
-class Args:
-# The directory where the images are (subfolders are included)
-    top_level_image_directory = r"C:\Users\chris\Documents\GitHub\ComfyUI_windows_portable\ComfyUI\output\training4"
-# How strongly to prefer images that have been shown less. 0 = totally random, 0.999 = very very strong preference. 0.4 is fine.
-    low_count_weight =  0.4
-# Preferred height of the window on your screen  
-    height = 768
-# How many comparisons to do each time you run the script. 
-    max_comparisons = 100
-# How many images to compare each time. Strongly suggest you leave as 2.
-    image_count = 2
-# List the highest rated n images at the end (0 or None for off)
-    show_top_n = 10
-# how rapidly to update scores. k = 0.7 has some theoretical basis!
-    k = 0.7
-    weight_k_by_speed = False
-    default_seconds = 1.2
-    weighting_limits = (0.5, 2.0)
-
-# Scores file (in the top_level_image_directory) to load from. Set to "" or None to start without any scores.
-    load_from = "start_from_model.json"
-# Name under which to save the scores at the end. An additional file, `same_name_x.json` (where x is number of comparisons to date) is also saved.
-    score_filename = "start_from_model.json"
-# Save a csv file as well?
-    csv_filename = None
-
 from modules.scoring import ImageDatabase, ImageRecord, ScoreUpdater
 
-def clamp(n, min, max): 
-    if n < min: return min
-    elif n > max: return max
-    else: return n 
+def parse_arguments():
+    parser = argparse.ArgumentParser("Score a set of images by a series of AB comparisons")
+    parser.add_argument('-d', '--directory', help="Top level directory", required=True)
+    parser.add_argument('-s', '--scores', default="scores.json", help="Filename of scores file (relative to top level directory) from which scores are loaded (if present) and saved")
+    parser.add_argument('-r', '--restart', action="store_true", help="Force a restart (don't reload scores file even if present)")
+    parser.add_argument('-c', '--csvfile', help="Save scores as a csv in this file (relative to top level directory) as well as in the scores file")
+
+    parser.add_argument('--lcw', type=float, default=0.4, help="Weighting priority towards less frequently compared images (0-0.99)")
+    parser.add_argument('--height', type=int, default=768, help="Height of window")
+    parser.add_argument('--number', type=int, default=100, help="Number of sets of images to compare")
+    parser.add_argument('--number_to_compare', type=int, default=2, help="Number of images to choose from")
+
+    parser.add_argument('--k', type=float, default=0.7, help="K value for score updates")
+    parser.add_argument('--weight_by_speed', action="store_true", help="Weight fast responses more (see also --default_seconds, --weight_min, --weight_max)")
+    parser.add_argument('--default_seconds', type=float, default=1.5, help="Typical response time (requires --weight_by_speed)")
+    parser.add_argument('--weight_min', type=float, default=0.5, help="Minimum weighting for slow responses (requires --weight_by_speed)")
+    parser.add_argument('--weight_max', type=float, default=2, help="Maximum weighting for fase responses (requires --weight_by_speed)")
+
+    Args.namespace = parser.parse_args()
+
+class Args:
+    namespace = None
+    @classmethod
+    def __getattribute__(cls, attr):
+        if hasattr(cls.namespace, attr): return cls.namespace.attr
+        if attr=='load_from': return None if cls.namespace.restart else cls.namespace.scores
+        raise Exception(f"Don't know about {attr}")
+
+def clamp(n, min, max): return min if n < min else (max if n > max else n)
 
 class ImageChooser:
     def __init__(self, image_records:list[ImageRecord], weighter:callable):
@@ -66,20 +65,20 @@ class TheApp:
     def __init__(self):
         self.app = customtkinter.CTk()
         self.app.title("")
-        self.database = ImageDatabase(Args.top_level_image_directory, loadfrom=Args.load_from)
+        self.database = ImageDatabase(Args.directory, loadfrom=Args.load_from)
 
         self.database.sort(reverse=True)
         self.start_order = { f:i for i,f in enumerate(self.database.image_records) }
 
         print(f"Comparing {len(self.database.records)} images")
         assert len(self.database.records) >= 2
-        self.image_chooser = ImageChooser.from_database(self.database, low_count_weight=Args.low_count_weight)
+        self.image_chooser = ImageChooser.from_database(self.database, low_count_weight=Args.lcw)
         self.score_updater = ScoreUpdater(Args.k)
         self.count = 0
 
         maw = self.database.max_aspect_ratio()
-        self.app.geometry(f"{Args.height*maw*Args.image_count}x{Args.height}")
-        self.image_labels = [customtkinter.CTkLabel(self.app, text="") for _ in range(Args.image_count)]
+        self.app.geometry(f"{Args.height*maw*Args.number_to_compare}x{Args.height}")
+        self.image_labels = [customtkinter.CTkLabel(self.app, text="") for _ in range(Args.number_to_compare)]
         for i, label in enumerate(self.image_labels):
             label.grid(row=0, column=2*i)
             if i: self.app.grid_columnconfigure(2*i-1, weight=1)
@@ -91,7 +90,7 @@ class TheApp:
         
 
     def pick_images(self):
-        self.image_records = self.image_chooser.pick_images(Args.image_count)
+        self.image_records = self.image_chooser.pick_images(Args.number_to_compare)
         for i, image_record in enumerate(self.image_records):
             im = self.database.get_image(image_record)
             try:
@@ -101,19 +100,19 @@ class TheApp:
         self.lasttime = time.monotonic()
 
     def keyup(self,k):
-        if k.char in "123456789"[:Args.image_count+1]: 
+        if k.char in "123456789"[:Args.number_to_compare+1]: 
             time_taken = time.monotonic() - self.lasttime
-            k_fac = clamp(Args.default_seconds / time_taken, *Args.weighting_limits) if Args.weight_k_by_speed else 1.0
+            k_fac = clamp(Args.default_seconds / time_taken, Args.weight_min, Args.weight_max) if Args.weight_by_speed else 1.0
             win = int(k.char)-1
-            for i in range(Args.image_count):
+            for i in range(Args.number_to_compare):
                 if i!=win: self.score_updater.update_scores(winner = self.image_records[win], loser=self.image_records[i], k_fac=k_fac)
             self.count += 1
             self.pick_images()
-        if self.count>=Args.max_comparisons or k.char=='q':
+        if self.count>=Args.number or k.char=='q':
             self.database.sort(reverse=True)
-            self.database.save_scores(Args.score_filename)
-            if Args.csv_filename: self.database.save_csv(Args.csv_filename)
-            if Args.show_top_n: print("\n".join((ir.printable for ir in self.database.records[:Args.show_top_n])))
+            self.database.save_scores(Args.scores)
+            if Args.csvfile: self.database.save_csv(Args.csvfile)
+
             summary = self.database.printable + " " + self.score_updater.printable
             print(summary)
             with open('summary.txt','a') as f: print(summary,file=f)
@@ -123,9 +122,10 @@ class TheApp:
                                              [ self.start_order[f] for f in self.database.image_records ])
             print("spearman correlation start to end of run: {:>6.4f}".format(spearman.statistic))
             self.app.quit()
-        self.app.title("{:>4}/{:<4} {:>6.3f} s/image".format(self.count, Args.max_comparisons, (time.monotonic()-self.starttime)/self.count))
+        self.app.title("{:>4}/{:<4} {:>6.3f} s/image".format(self.count, Args.number, (time.monotonic()-self.starttime)/self.count))
 
 def main():
+    parse_arguments()
     a = TheApp()
     a.app.mainloop()
     hist = [0]*1000
